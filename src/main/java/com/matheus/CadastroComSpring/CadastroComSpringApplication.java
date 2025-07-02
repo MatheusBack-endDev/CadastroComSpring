a958a7c628db28423197551300d9a600ec4fc158
MatheusBack-endDev <matheusmdevops@gmail.com>
2025-07-01 21:07:57 -0300

    firt commit

---
+++ src/main/java/com/matheus/CadastroComSpring/CadastroComSpringApplication.java
@@ -0,0 +1,13 @@
+package com.matheus.CadastroComSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroComSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroComSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matheus/CadastroComSpring/CadastroComSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheus.CadastroComSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroComSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
